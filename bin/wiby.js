792a81ab3c3522db7564e5dd733bf1a9d3b463f0
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-05-26 10:49:47 +0100

    file: initial structure

---
+++ bin/wiby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const test = require('../lib/test');
+const result = require('../lib/result');
+
+if (process.argv[2] === "test") {
+  test();
+}
+
+if (process.argv[2] === "result") {
+  result();
+}
