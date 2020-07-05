0ab66048a6cf46a8d83047fc86e799d95efc8ec1
Dominykas Blyžė <hello@dominykas.com>
2020-07-05 17:56:38 +0300

    test: disable networking related tests when no GITHUB_TOKEN available

--- test/github.js
+++ test/github.js
@@ -3,11 +3,13 @@ const tap = require('tap')
 const github = require('../lib/github')
 const CONFIG = require('./fixtures/config')
 
-tap.test('package.json can be fetched with a valid url', async tap => {
-  tap.equal(JSON.stringify(await github.getPackageJson(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), JSON.stringify(CONFIG.PKGJSON))
-  // tap.throws( await pkgTest.getPackageJson('not-an-org', 'not-a-repo'))
-})
+if (process.env.GITHUB_TOKEN) {
+  tap.test('package.json can be fetched with a valid url', async tap => {
+    tap.equal(JSON.stringify(await github.getPackageJson(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), JSON.stringify(CONFIG.PKGJSON))
+    // tap.throws( await pkgTest.getPackageJson('not-an-org', 'not-a-repo'))
+  })
 
-tap.test('correct permissions returned for GitHub repo', async tap => {
-  tap.equal((await github.getPermissions(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), CONFIG.DEP_REPO_PERM)
-})
+  tap.test('correct permissions returned for GitHub repo', async tap => {
+    tap.equal((await github.getPermissions(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), CONFIG.DEP_REPO_PERM)
+  })
+}
