0d5460dcb124a38f1ab48f9c1e13eac07235c194
Dominykas Blyžė <hello@dominykas.com>
2020-07-06 21:48:36 +0300

    test: use options.skip available in tap

--- test/github.js
+++ test/github.js
@@ -3,13 +3,11 @@ const tap = require('tap')
 const github = require('../lib/github')
 const CONFIG = require('./fixtures/config')
 
-if (process.env.GITHUB_TOKEN) {
-  tap.test('package.json can be fetched with a valid url', async tap => {
-    tap.equal(JSON.stringify(await github.getPackageJson(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), JSON.stringify(CONFIG.PKGJSON))
-    // tap.throws( await pkgTest.getPackageJson('not-an-org', 'not-a-repo'))
-  })
+tap.test('package.json can be fetched with a valid url', async tap => {
+  tap.equal(JSON.stringify(await github.getPackageJson(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), JSON.stringify(CONFIG.PKGJSON))
+  // tap.throws( await pkgTest.getPackageJson('not-an-org', 'not-a-repo'))
+}, { skip: !process.env.GITHUB_TOKEN })
 
-  tap.test('correct permissions returned for GitHub repo', async tap => {
-    tap.equal((await github.getPermissions(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), CONFIG.DEP_REPO_PERM)
-  })
-}
+tap.test('correct permissions returned for GitHub repo', async tap => {
+  tap.equal((await github.getPermissions(CONFIG.DEP_ORG, CONFIG.DEP_REPO)), CONFIG.DEP_REPO_PERM)
+}, { skip: !process.env.GITHUB_TOKEN })
