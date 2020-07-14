5a124183b53d432170315b541d864a3cdae12a88
Beth Griggs <Bethany.Griggs@uk.ibm.com>
2020-07-14 13:56:17 +0100

    test: patch tests for refactor

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs').promises
 const tap = require('tap')
 const path = require('path')
 const CONFIG = require('./fixtures/config')
@@ -15,33 +16,14 @@ tap.test('Check if the dependency is listed in the package.json', tap => {
   tap.end()
 })
 
-tap.test('GitHub organisation/owner and repository returned', tap => {
-  tap.equal(pkgTest.getOrgRepo(CONFIG.DEP_URL)[0], CONFIG.DEP_ORG)
-  tap.equal(pkgTest.getOrgRepo(CONFIG.DEP_URL)[1], CONFIG.DEP_REPO)
-  tap.end()
+tap.test('commit url created from github org repo and commit sha', async tap => {
+  tap.equal(await pkgTest.getCommitURL(CONFIG.PKG_ORG, CONFIG.PKG_NAME, CONFIG.PKG_HEAD_SHA), CONFIG.PATCH)
 })
 
-tap.test('patch created from github org repo and commit sha', async tap => {
-  tap.equal(await pkgTest.getPatch(CONFIG.PKG_NAME, CONFIG.PKG_HEAD_SHA), CONFIG.PATCH)
-})
-
-tap.test('Local package.json name returned correctly', async tap => {
+tap.test('Local package.json returned correctly', async tap => {
   const pkgPath = path.join(__dirname, '/fixtures/package.json')
-  tap.equal(await pkgTest.getLocalPackageName(pkgPath), localPkg.name)
-})
-
-tap.test('GitHub org and repo returned when given package name', async tap => {
-  tap.equal((await pkgTest.getGitHubOrgRepo(CONFIG.PKG_NAME))[0], CONFIG.PKG_ORG)
-  tap.equal((await pkgTest.getGitHubOrgRepo(CONFIG.PKG_NAME))[1], CONFIG.PKG_REPO)
-})
-
-tap.test('Check registry info is fetched correctly', async tap => {
-  tap.equal((await pkgTest.fetchRegistryInfo(CONFIG.PKG_NAME))._id, CONFIG.PKG_NAME)
-})
-
-tap.test('undefined returned when given an invalid package name', async tap => {
-  tap.equal((await pkgTest.getGitHubOrgRepo('not-a-package'))[0], 'undefined')
-  tap.equal((await pkgTest.getGitHubOrgRepo('not-a-package'))[1], 'undefined')
+  const expectedPackageJSON = await fs.readFile(pkgPath)
+  tap.looseEqual(await pkgTest.getLocalPackageJSON(pkgPath), JSON.parse(expectedPackageJSON))
 })
 
 tap.test('Check patch applied to package.json successfully', tap => {
