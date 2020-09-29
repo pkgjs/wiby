6128b8535fe3384485b2262c50bc20870595e078
Dominykas Blyžė <hello@dominykas.com>
2020-09-29 18:43:18 +0300

    feat: add validation basics

--- test/config.js
+++ test/config.js
@@ -1,9 +1,43 @@
 'use strict'
 
+const path = require('path')
 const tap = require('tap')
 const wiby = require('..')
 
-tap.test('Exists', (tap) => {
+const invalidConfigs = {
+  'fail-bad-json.json': 'Unexpected end of JSON input',
+  'fail-unknown-root-key.json': '"not-allowed" is not allowed',
+  'fail-unknown-dependent-key.json': '"dependents[0].sub-key-not-allowed" is not allowed',
+  'fail-dependent-not-url.json': '"dependents[0].repository" must be a valid uri'
+}
+
+const validConfigs = [
+  'pass-empty.json',
+  'pass-empty-dependents.json',
+  'pass-wiby.js'
+]
+
+tap.test('should pass with the .wiby.json of the wiby itself', (tap) => {
   wiby.validate({})
   tap.end()
+})
+
+tap.test('Invalid configs', async (tap) => {
+  for (const [file, expectedError] of Object.entries(invalidConfigs)) {
+    tap.test(file, (tap) => {
+      tap.throws(() => {
+        wiby.validate({ config: path.join(__dirname, 'fixtures', 'config', file) })
+      }, { message: expectedError })
+      tap.end()
+    })
+  }
+})
+
+tap.test('Valid configs', async (tap) => {
+  for (const file of validConfigs) {
+    tap.test(file, (tap) => {
+      wiby.validate({ config: path.join(__dirname, 'fixtures', 'config', file) })
+      tap.end()
+    })
+  }
 })
